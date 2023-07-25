601f25bd1d6d9ec60d57f580cfd3b862f1a9ee12
Davi Cardoso <20673538+daviccardoso@users.noreply.github.com>
2023-07-25 17:56:40 -0300

    Project structure and metadata setup (#11)
    - Add __init__.py to turn the project into a package
    - Add __main__.py to turn the package executable
    - Add setup.py to turn the package installable
    - Add folder for documentation
    
    Closes #8

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dundie",
+    version="0.1.0",
+    description="Reward system for employees of Dunder Mifflin Paper Company",
+    author="Davi Cardoso",
+    packages=find_packages(),
+)
