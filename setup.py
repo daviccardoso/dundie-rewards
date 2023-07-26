d114f239d056d5c3f0ba6df6300837d94cbe036d
Davi Cardoso <20673538+daviccardoso@users.noreply.github.com>
2023-07-26 10:36:29 -0300

    Set up the package to be a installable binary

--- setup.py
+++ setup.py
@@ -6,4 +6,5 @@ setup(
     description="Reward system for employees of Dunder Mifflin Paper Company",
     author="Davi Cardoso",
     packages=find_packages(),
+    entry_points={"console_scripts": ["dundie = dundie.__main__:main"]},
 )
